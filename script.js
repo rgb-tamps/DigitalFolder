fa4b2ad58fede9fbf57c47a7392c81fc1be12ded
rgb-tamps <141804306+rgb-tamps@users.noreply.github.com>
2026-02-21 15:46:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById("hamburgerButton").addEventListener("click", function() {
+    const menu = document.getElementById("menu");
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+
+    document.getElementById("myButton").addEventListener("click", function() {
+    });
+
+});
+    
